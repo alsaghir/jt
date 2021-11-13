4af6181a9345eedeee34ad4d593c5f3ff5a0c54e
Al-Saghir <ahmedalsaghirxyz@gmail.com>
2021-11-13 13:48:10 +0200

    initial commit

---
+++ src/test/java/com/github/alsaghir/jt/JtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alsaghir.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
